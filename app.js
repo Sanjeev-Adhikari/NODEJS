a6864cee5bc319e093e7450026f167b28dadac10
Sanjeev Adhikari <sanjeevadhikari75@gmail.com>
2024-07-19 16:56:37 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//console.log("welcome to nodejs")
+
+// array
+
+
+const numbers = [10,20,30,40,50]
+//console.log(numbers[1])
+
+const person = ["sanjeev", "anand", "aman", "anuj"]
+
+//console.log(person[0])
+
+//Object
+
+const names = {
+    name : "sanjeev",
+    age : 25,
+    status : "unmarried"
+
+}
+
+console.log("His name is " + names.name, "he is " + names.age, "and he is " + names.status)
